40c47bc53726dd57556d662c8ba6392b3bad122e
David Basil <info@basildavid.com>
2017-12-17 08:45:48 +0400

    add external phone-list template

--- phone-list/phone-list.component.js
+++ phone-list/phone-list.component.js
@@ -1,23 +1,23 @@
 ;(function(){
 
 	var phoneList = {
-		template: '<ul>' + '<li ng-repeat="phone in $ctrl.phones">' + '<span>{{phone.name}}</span>' + '<p>{{phone.snippet}}</p>' + '</li>' + '</ul><p>Total number of phones: {{$ctrl.phones.length}}</p>',
-			controller: function PhoneListController(){
-				this.phones = [
-					{
-						name: 'Nexus S',
-						snippet: 'Fast just got faster with Nexus S'
-					},
-					{
-						name: 'Motorola XOOM with Wi-Fi',
-						snippet: 'The Next, Next Generation tablet.'
-					},
-					{
-						name: 'Motorola XOOM',
-						snippet: 'The Next, Next Generation tablet.'
-					}
-				]
-			}
+		templateUrl: 'phone-list/phone-list.template.html',
+		controller: function PhoneListController(){
+			this.phones = [
+				{
+					name: 'Nexus S',
+					snippet: 'Fast just got faster with Nexus S'
+				},
+				{
+					name: 'Motorola XOOM with Wi-Fi',
+					snippet: 'The Next, Next Generation tablet.'
+				},
+				{
+					name: 'Motorola XOOM',
+					snippet: 'The Next, Next Generation tablet.'
+				}
+			]
+		}
 		}
 
 	angular.module('phoneList')
