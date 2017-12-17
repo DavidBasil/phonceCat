1eaec62d19a4069c91d98efc44edc5793bb5a9fd
David Basil <info@basildavid.com>
2017-12-17 09:45:49 +0400

    add angular-route

--- phone-list/phone-list.component.js
+++ phone-list/phone-list.component.js
@@ -1,24 +1,19 @@
 ;(function(){
 
+	function PhoneListController($http){
+		var self = this
+		self.orderProp = 'age'
+		$http.get('phones/phones.json')
+			.then(function(response){
+				self.phones = response.data.slice(0, 5)
+			})
+	}
+
+	PhoneListController.$inject = ['$http']
+
 	var phoneList = {
 		templateUrl: 'phone-list/phone-list.template.html',
-		controller: function PhoneListController(){
-			this.phones = [
-				{
-					name: 'Nexus S',
-					snippet: 'Fast just got faster with Nexus S'
-				},
-				{
-					name: 'Motorola XOOM with Wi-Fi',
-					snippet: 'The Next, Next Generation tablet.'
-				},
-				{
-					name: 'Motorola XOOM',
-					snippet: 'The Next, Next Generation tablet.'
-				}
-			]
-			this.orderProp = 'age'
-		}
+		controller: PhoneListController
 	}
 
 	angular.module('phoneList')
