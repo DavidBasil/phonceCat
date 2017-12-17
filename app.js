266e8269f2bd6ff762ce20285eab15fc46361237
David Basil <info@basildavid.com>
2017-12-17 08:18:53 +0400

    add a phone-list component

--- app.js
+++ app.js
@@ -3,24 +3,6 @@
 
 	angular
 		.module('phonecatApp', [])
-		.controller('PhoneListController', PhoneListController)
 
-	PhoneListController.$inject = ['$scope']
 
-	function PhoneListController($scope){
-		$scope.phones = [
-			{
-				name: 'Nexus S',
-				snippet: 'Fast just got faster with Nexus S.'
-			},
-			{
-				name: 'Motorola XOOM with wi-fi',
-				snippet: 'The Next, Next generation tablet.'
-			},
-			{
-				name: 'Motorola XOOM',
-				snippet: 'The Next, Next generation tablet.'
-			}
-		]
-	}
 })()
